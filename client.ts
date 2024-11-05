d611c0fb698407b5cdf8f6bfc48d1e704e707a69
“abbymccollam3” <amccollam3@gatech.edu>
2024-11-04 16:12:37 -0800

    User Auth

--- client.ts
+++ client.ts
@@ -1,1 +1,15 @@
+// Author: Abby McCollam
+// Challenge: Building Spotify Client
 
+/*
+Step 1: Authenticate the user of your client
+*/
+
+
+const generateRandomString = (length) => {
+    const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+    const values = crypto.getRandomValues(new Uint8Array(length));
+    return values.reduce((acc, x) => acc + possible[x % possible.length], "");
+  }
+  
+  const codeVerifier  = generateRandomString(64);  
